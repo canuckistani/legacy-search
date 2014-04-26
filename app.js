f60aea3ea634b00cc33aefc258598a1b6211419a
canuckistani <jeff@canuckistani.ca>
2014-04-25 18:03:35 -0700

    store the order in localforage

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 function fetchData(callback) {
-    $.getJSON('/mock/beers.json', function(json) {
-        initTable(json);
-    });
+  $.getJSON('/mock/beers.json', function(json) {
+    initTable(json);
+  });
 }
 
-// --
+function getAllItems(callback) {
+  var _i = 0, collected = [];
+  localforage.length().then(function(length) {
+    _.each(_.range(length), function(item, i) {
+      localforage.key(i).then(function(key) {
+        localforage.getItem(key).then(function(data) {
+          _i++;
+          collected.push(data);
+          if (_i === length) {
+            callback(collected);    
+          }
+        });
+      });
+    });
+  });
+}
 
-// Model for selected beers, using localStorage
-
-// --
+function initStorage(cb) {
+  getAllItems(function(results) {
+    _.each(results, function(item, index) {
+      displayItem(item._id, item);
+    });
+    cb(results);
+  });
+}
 
 var beersHashMap = {};
-var beerOrder = new EventEmitter();
 
 function initTable(json) {
   var data = _.map(json, function(a) {
@@ -51,32 +70,78 @@ function initTable(json) {
   });
 }
 
+function displayItem(id, selected) {
+  if (!selected.order_quantity) {
+    selected.order_quantity = 1;
+  }
+  $('ul#sel-beer-list').append('<li id="'+id+'">'+
+    '<a href="#">[X]</a> (<span class="item-quantity">'+selected.order_quantity+'</span>) '+
+    [selected.name, selected.price, selected.link].join(' ')+
+    '</li>');
+}
+
+function incrementItem(id) {
+  localforage.getItem(id, function(item) {
+    item.order_quantity++;
+    localforage.setItem(id, item, function() {
+      $('li#'+id+' span.item-quantity').html(item.order_quantity);
+    });
+  });
+}
+
+function exportOrder() {
+  var output = '';
+  getAllItems(function(order) {
+    _.each(order, function(beer) {
+      output += beer.order_quantity + 'x "' + beer.name + '" ( '+ beer.link +' ) '+ "\n";
+    });
+    console.log(output);
+  });
+}
+
 // event bindings
 $('#beer-table').click(function(ev) {
-    if (_.contains(ev.target.classList, 'add-link')) {
-        ev.preventDefault();
-        // console.log('got add-link, id is: '+ev.target.id);
-        var selected = beersHashMap[ev.target.id];
-        $('ul#sel-beer-list').append('<li id="'+ev.target.id+'">'+
-          '<a href="#">[X]</a> '+
-          [selected.name, selected.price, selected.link].join(' ')+
-          '</li>');
-        selected['_id'] = ev.target.id;
-        beerOrder.put(selected);
-    }
+  if (_.contains(ev.target.classList, 'add-link')) {
+    ev.preventDefault();
+    var selected = beersHashMap[ev.target.id];
+    selected['_id'] = ev.target.id;
+
+    localforage.getItem(ev.target.id, function(item) {
+      if (!item) {
+        selected.order_quantity = 1;
+        localforage.setItem(ev.target.id, selected, function() {
+          console.log([].slice.call(arguments));
+          console.log('item '+ ev.target.id+' stored!');
+          // display the item
+          displayItem(ev.target.id, selected);
+        });  
+      }
+      else {
+        incrementItem(ev.target.id);
+      }
+    });
+  }
 });
 
 $('#clear-selected').click(function() {
-  $('ul#sel-beer-list').html('');
+  localforage.clear(function() {
+    $('ul#sel-beer-list').html('');
+  });
 });
 
 $('ul#sel-beer-list').click(function(ev) {
   ev.preventDefault();
-  $(ev.target).parent().remove();
-
+  console.log(ev.target.parentElement.id);
+  localforage.removeItem(ev.target.parentElement.id, function() {
+    $(ev.target).parent().remove();
+  });
 });
 
+// Initialization
 $(function() {
-  // beerOrder = new PouchDB('beerOrder');
+  initStorage(function(data) {
+    console.log('storage initialized...');
+    console.log(data);
+  });
   fetchData(initTable);
 });
